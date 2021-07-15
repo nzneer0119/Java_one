5176d4268acc657632dacec41b5c31a67d30434b
limha <limha@LAPTOP-DCUHFA64>
2021-07-15 21:53:34 +0900

    chapter 3 예제 추가

--- one/src/chap04/Homework01.java
+++ one/src/chap04/Homework01.java
@@ -1,8 +1,10 @@
 package chap04;
 
+import java.io.IOException;
+
 public class Homework01 {
 
-	public static void main(String[] args)  throws Exception{
+	public static void main(String[] args)  throws IOException{
 		boolean run = true;
         int keyCode = 0;
         
@@ -14,26 +16,25 @@ public class Homework01 {
                  System.out.println("중지 하려면 숫자 0을 누르세요.");     
             	 System.out.println("----------------------------------");
                  System.out.print("입력 : "); 
-             }
-             
+             }             
              keyCode = System.in.read();
-            
+             	{
             if ((keyCode >=49) && (keyCode <=57) )  {
                 System.out.println("숫자입니다.");
             }
            	 else if ((keyCode >=65) && (keyCode <=90)) {
             	 System.out.println("문자입니다.");
-            }
+           	}
              else if ((keyCode >=97) && (keyCode <=122)) {
             	 System.out.println("문자입니다.");
-            }
+           	}
              else if ((keyCode ==48)) {
              	 run = false;
-           }            
-             else {
+           	}  else {
             	System.out.println("특수문자입니다.");
-            }
-        }
+           	}
+       }
+     }
         System.out.println("종료합니다.");
      }
 }
