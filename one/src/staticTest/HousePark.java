4ca6a88ff25794e4a52cd89a27f6975c6e800117
limha <limha@LAPTOP-DCUHFA64>
2021-07-17 21:48:48 +0900

    static 예제 연습

---
+++ one/src/staticTest/HousePark.java
@@ -0,0 +1,11 @@
+package staticTest;
+
+public class HousePark {
+	static String lastname = "박";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HousePark pey = new HousePark();
+        HousePark pes = new HousePark();
+	}
+}
