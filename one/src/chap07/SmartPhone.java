599aefa00bc28755cece02d6b3a2922b0cc8acb9
504 <504@DESKTOP-B0KVD9C>
2021-07-20 16:46:31 +0900

    chapter 07 완료

---
+++ one/src/chap07/SmartPhone.java
@@ -0,0 +1,13 @@
+package chap07;
+
+public class SmartPhone extends Phone{
+	// 생성자
+	public SmartPhone (String owner) {
+		super(owner);
+	}
+	
+	// 메소드
+	public void internetSearch() {
+		System.out.println(owner + "인터넷을 검색합니다.");
+	}
+}
