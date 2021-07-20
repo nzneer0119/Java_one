d1b226ef28fd9814339080502ac3de6a8d954539
504 <504@DESKTOP-B0KVD9C>
2021-07-20 18:01:28 +0900

    chapter 8 abstract interface / implements 예제

---
+++ one/src/chap08/RemoteControlExample.java
@@ -0,0 +1,34 @@
+package chap08;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*RemoteControl rc = new RemoteControl() {
+
+			@Override                     // RemoteControl에서 상속 받았으면 (개선해서)사용 해라..
+			public void turnOn() {
+			}
+			@Override
+			public void turnOff() {
+			}
+			@Override
+			public void setVolume(int volume) {
+			}
+		*/
+		RemoteControl rc = null;
+			
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.setMute(true);
+		rc.setMute(false);
+		rc.turnOff();
+
+	};
+
+	}
+
